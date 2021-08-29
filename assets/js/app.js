651651e7d93f0c6981de983f5720f7e922ce58f2
Abhinav Shrestha <79868048+abhikenobi@users.noreply.github.com>
2021-08-29 13:20:47 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,4 +26,56 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Create 
+// Create Graph
+
+//Read the data
+d3.csv("assets/data/data.csv").then(censusdata => {
+
+    // Take your two variables from the csv
+    censusdata.forEach(data => {
+        data.poverty = +data.poverty;
+        data.income =  +data.income;
+    });
+
+    // Initial axis params
+    var xaxis = "income";
+    var yaxis = "poverty";
+
+    // X and Y axis scales
+    var xScale = d3.scaleLinear().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
+});
+
+
+
+
+
+// , function(data) {
+
+//   // Add X axis
+//   var x = d3.scaleLinear()
+//     .domain([0, 4000])
+//     .range([ 0, width ]);
+//   svg.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x));
+
+//   // Add Y axis
+//   var y = d3.scaleLinear()
+//     .domain([0, 500000])
+//     .range([ height, 0]);
+//   svg.append("g")
+//     .call(d3.axisLeft(y));
+
+//   // Add dots
+//   svg.append('g')
+//     .selectAll("dot")
+//     .data(data)
+//     .enter()
+//     .append("circle")
+//       .attr("cx", function (d) { return x(d.GrLivArea); } )
+//       .attr("cy", function (d) { return y(d.SalePrice); } )
+//       .attr("r", 1.5)
+//       .style("fill", "#69b3a2")
+
+// })
